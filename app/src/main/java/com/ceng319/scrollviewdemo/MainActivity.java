3a3d224194e3007b05eadabf62abc876f5cc38d4
HusnalK <54862075+HusnalK@users.noreply.github.com>
2019-12-08 00:46:55 -0500

    Initial commit

---
+++ app/src/main/java/com/ceng319/scrollviewdemo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ceng319.scrollviewdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
